cb7fe128111a822c16ac9396a5ce80c310c7efd6
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-04-11 13:36:31 +0200

    Add '+' operator

--- R/operators.R
+++ R/operators.R
@@ -21,12 +21,7 @@ setMethod(
   f = "-",
   signature(e1 = "GammaSpectrum", e2 = "GammaSpectrum"),
   definition = function (e1, e2) {
-    if (e1@reference != e2@reference |
-        e1@date != e2@date |
-        e1@instrument != e2@instrument |
-        e1@file_format != e2@file_format |
-        e1@live_time != e2@live_time |
-        e1@real_time != e2@real_time)
+    if (e1@hash != e2@hash)
       stop("Spectrum and baseline do not match.")
 
     methods::new(
@@ -37,7 +32,32 @@ setMethod(
       file_format = e1@file_format,
       chanel = e2@chanel,
       energy = e2@energy,
-      counts = e1@counts[e2@chanel] - e2@counts,
+      counts = e1@counts - e2@counts,
+      live_time = e1@live_time,
+      real_time = e1@real_time
+    )
+  }
+)
+
+#' @export
+#' @rdname operators
+#' @aliases +,GammaSpectrum,GammaSpectrum-method
+setMethod(
+  f = "+",
+  signature(e1 = "GammaSpectrum", e2 = "GammaSpectrum"),
+  definition = function (e1, e2) {
+    if (e1@hash != e2@hash)
+      stop("Spectrum and baseline do not match.")
+
+    methods::new(
+      "GammaSpectrum",
+      reference = e1@reference,
+      date = e1@date,
+      instrument = e1@instrument,
+      file_format = e1@file_format,
+      chanel = e2@chanel,
+      energy = e2@energy,
+      counts = e1@counts + e2@counts,
       live_time = e1@live_time,
       real_time = e1@real_time
     )
