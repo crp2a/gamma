388faabfd989ef7f0826a1c6a571b6852519a86a
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-04-26 16:09:25 +0200

    Save CRP2A calibration curve (LaBr)

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' CRP2A Calibration Curve #1
+#'
+#' @details
+#' \tabular{ll}{
+#'  \strong{Laboratory:} \tab IRAMAT-CRP2A (UMR 5060) \cr
+#'  \strong{Instrument:} \tab Canberra Inspector 1000 \cr
+#'  \strong{Detector:} \tab LaBr \cr
+#'  \strong{Authors:} \tab CRP2A Luminescence Team
+#' }
+#' @section Changelog:
+#'  \describe{
+#'   \item{v0.1}{Curve creation.}
+#'  }
+#' @examples
+#' # Load the curve
+#' utils::data("BDX1", package = "gamma")
+#' BDX1
+#'
+#' # See how to replicate the curve
+#' utils::vignette("CRP2A #1", package = "gamma")
+"BDX1"
