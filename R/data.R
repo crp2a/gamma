a36ffd58e57f2b2f874f6dfd74fbbee515e1228b
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-05-20 17:41:45 +0200

    Add CEREGE calibration curve

--- R/data.R
+++ R/data.R
@@ -1,3 +1,25 @@
+#' CEREGE Calibration Curve #1
+#'
+#' @details
+#' \tabular{ll}{
+#'  \strong{Laboratory:} \tab CEREGE \cr
+#'  \strong{Instrument:} \tab Canberra Inspector 1000 \cr
+#'  \strong{Detector:} \tab NaI \cr
+#'  \strong{Authors:} \tab CEREGE Luminescence Team
+#' }
+#' @section Changelog:
+#'  \describe{
+#'   \item{v0.1.1}{Curve creation.}
+#'  }
+#' @examples
+#' # Load the curve
+#' utils::data("AIX1", package = "gamma")
+#' AIX1
+#'
+#' # See how to replicate the curve
+#' utils::vignette("AIX1#1", package = "gamma")
+"AIX1"
+
 #' CRP2A Calibration Curve #1
 #'
 #' @details
