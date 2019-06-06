f29842cc01310a70b009eb0c79b40a2282f53218
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-06-06 17:30:05 +0200

    Add helpers

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,50 @@
 # HELPERS
 
+#' Helpers
+#'
+#' \code{compact} removes elements from a list or vector.
+#' \code{detect} xxx.
+#'
+#' %o% allows for function composition.
+#'
+#' %||% xxx.
+#' @param x An object.
+#' @param f,f1,f2 A \code{\link{function}}. In \code{compact} and \code{detect}
+#'  \code{f} must be a logical predicate.
+#' @param g A \code{\link{function}}.
+#' @param lhs An object.
+#' @param rhs An object.
+#' @details
+#'  Adapted from H. Wickham's \emph{Avanced R}.
+#' @return
+#'  TODO
+#' @references
+#'  Wickham, H. (2014). \emph{Advanced R}. London: Chapman & Hall. The R Series.
+#' @name helpers
+#' @keywords internal
+NULL
+
+#' @rdname helpers
+`%||%` <- function(lhs, rhs) {
+  if (!is.null(lhs)) lhs else rhs
+}
+#' @rdname helpers
+`%o%` <- function(f, g) {
+  function(...) f(g(...))
+}
+#' @rdname helpers
+compact <- function(f, x) {
+  Filter(Negate(f), x)
+}
+#' @rdname helpers
+detect <- function(f, x) {
+  vapply(x, f, logical(1))
+}
+#' @rdname helpers
+count <- function(f, x) {
+  sum(detect(f, x))
+}
+
 #' Find nearest value
 #'
 #' Finds the closest element to a number in a vector.
@@ -39,6 +84,9 @@ isEqual <- function(x, tolerance = .Machine$double.eps^0.5, na.rm = TRUE) {
     stop("A numeric vector is expected.", call. = FALSE)
   abs(max(x, na.rm = na.rm) - min(x, na.rm = na.rm)) <= tolerance
 }
+isEmpty <- function(x) {
+  length(x) == 0
+}
 
 #' Positive numbers
 #'
@@ -60,4 +108,29 @@ isPositive <- function(x, strict = FALSE, na.rm = TRUE) {
   } else {
     !any(x < 0, na.rm = na.rm)
   }
+}
+
+#' Error
+#'
+#' Checks if an object only contains positive values.
+#' @param x A \code{\link{numeric}} object to be checked.
+#' @param strict A \code{\link{logical}} scalar.
+#' @param na.rm A \code{\link{logical}} scalar specifying if missing values
+#'  (including NaN) should be omitted.
+#' @return A \code{\link{logical}}.
+#' @keywords internal
+#' @noRd
+isError <- function(x) {
+  inherits(x, "error")
+}
+stopCustom <- function(.subclass, message, call = NULL, ...) {
+  error <- structure(
+    list(
+      message = message,
+      call = call,
+      ...
+    ),
+    class = c(.subclass, "error", "condition")
+  )
+  stop(error)
 }
